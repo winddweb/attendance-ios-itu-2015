8c76f8b54a28d0e7e0db04e45a8b9b7aecc5d956
winddweb <winddweb@gmail.com>
2015-11-11 07:35:47 -0800

    Added color definition Added tint color for app, tabor, tool bar, navigation bar

--- attendance/AppDelegate.swift
+++ attendance/AppDelegate.swift
@@ -15,7 +15,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        /*:
+        ## customize app colors
+        # colors
+        - orange #ff7302
+        - greenD1 #00622e
+        - greenD2 #01833e
+        - greenL1 #94bf7d
+        - greenL2 #bbdba6
+        */
+
+        // let orange = UIColor(red:1.00, green:0.45, blue:0.01, alpha:1.0)
+        let greenD1 = UIColor(red:0.00, green:0.38, blue:0.18, alpha:1.0)
+        let greenD2 = UIColor(red:0.00, green:0.51, blue:0.24, alpha:1.0)
+        // let greenL1 = UIColor(red:0.58, green:0.75, blue:0.49, alpha:1.0)
+        // let greenL2 = UIColor(red:0.73, green:0.86, blue:0.65, alpha:1.0)
+        let white = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+        // let grey = UIColor(red:0.55, green:0.55, blue:0.55, alpha:1.0)
+        // let black = UIColor(red:0.07, green:0.07, blue:0.07, alpha:1.0)
+        
+
+        // change global tint color
+        self.window?.tintColor = greenD1
+        
+        // change tint color of navigation bar items
+        UINavigationBar.appearance().tintColor = white
+        
+        // change tint color of navigation bar background
+        UINavigationBar.appearance().barTintColor = greenD2
+        
+        // change tint color of tool bar items
+        UIBarButtonItem.appearance().tintColor = white
+        
+        // change tint color of tool bar background
+        UIToolbar.appearance().barTintColor = greenD1
+        
+        // change tint color of tab bar items
+        UITabBar.appearance().tintColor = greenD2
+        
+        // change tint color of tab bar background
+        UITabBar.appearance().barTintColor = white
+        
+        
         return true
     }
 
